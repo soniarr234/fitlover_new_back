782e7e26ffdef16c68ccc8bc5f080d86d47f5eb7
Rodriguez Ramos Sonia <sonia.rodriguez@soprasteria.com>
2025-07-30 14:45:20 +0200

    Elimina node_modules y .env del repo

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API backend funcionando');
+});
+
 app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3001;
