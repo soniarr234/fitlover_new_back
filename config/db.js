aa7a736dbf283892cd1932b7458c43b4972a12bd
Rodriguez Ramos Sonia <sonia.rodriguez@soprasteria.com>
2025-07-31 11:20:39 +0200

    Conexión con Clever Cloud y despliegue en Render

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  port: process.env.MYSQL_ADDON_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
